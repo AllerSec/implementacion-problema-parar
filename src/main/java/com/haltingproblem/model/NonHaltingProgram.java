6200cad34f183e2f4de32e598ba4e959d36cebb7
pabloloz23 <145718438+pabloloz23@users.noreply.github.com>
2025-03-19 11:59:27 +0100

    Update NonHaltingProgram.java

--- src/main/java/com/haltingproblem/model/NonHaltingProgram.java
+++ src/main/java/com/haltingproblem/model/NonHaltingProgram.java
@@ -1,4 +1,19 @@
 package com.haltingproblem.model;
 
-public class NonHaltingProgram {
+public class NonHaltingProgram extends Program {
+    @Override
+    public void run() {
+        int num = initialValue;
+        while(true) {
+            System.out.println(num);
+            num++;
+        }
+    }
+
+    @Override
+    public ProgramPrototype clone() {
+        NonHaltingProgram clone = new NonHaltingProgram();
+        clone.setInitialValue(this.initialValue);
+        return clone;
+    }
 }
