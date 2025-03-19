6cda244eaf59c1547c0232f2ab06ca57c61855da
pabloloz23 <145718438+pabloloz23@users.noreply.github.com>
2025-03-19 12:51:18 +0100

    Update Program.java

--- src/main/java/com/haltingproblem/model/Program.java
+++ src/main/java/com/haltingproblem/model/Program.java
@@ -1,8 +1,6 @@
 package com.haltingproblem.model;
 
-import com.haltingproblem.prototype.ProgramPrototype;
-
-public abstract class Program implements ProgramPrototype {
+public abstract class Program {
     protected int initialValue;
 
     public void setInitialValue(int value) {
