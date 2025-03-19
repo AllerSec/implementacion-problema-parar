efde6357c11a1f4cc44a36d5d8d6a517ece92fd1
pabloloz23 <145718438+pabloloz23@users.noreply.github.com>
2025-03-19 11:59:04 +0100

    Update HaltingProgram.java

--- src/main/java/com/haltingproblem/model/HaltingProgram.java
+++ src/main/java/com/haltingproblem/model/HaltingProgram.java
@@ -1,4 +1,19 @@
 package com.haltingproblem.model;
 
-public class HaltingProgram {
+public class HaltingProgram extends Program {
+    @Override
+    public void run() {
+        int num = initialValue;
+        while(num > 0) {
+            System.out.println(num);
+            num--;
+        }
+    }
+
+    @Override
+    public ProgramPrototype clone() {
+        HaltingProgram clone = new HaltingProgram();
+        clone.setInitialValue(this.initialValue);
+        return clone;
+    }
 }
