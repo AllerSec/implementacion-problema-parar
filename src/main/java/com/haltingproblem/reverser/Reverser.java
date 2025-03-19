0e745c0cffcb6e5417c45796626fbce026c61d3f
pabloloz23 <145718438+pabloloz23@users.noreply.github.com>
2025-03-19 11:58:12 +0100

    Update Reverser.java

--- src/main/java/com/haltingproblem/reverser/Reverser.java
+++ src/main/java/com/haltingproblem/reverser/Reverser.java
@@ -1,4 +1,29 @@
 package com.haltingproblem.reverser;
 
-public class Reverser {
+import com.haltingproblem.haltchecker.HaltChecker;
+import com.haltingproblem.model.Program;
+
+public class Reverser extends Program {
+    private Program program;
+
+    public Reverser(Program program) {
+        this.program = program;
+    }
+
+    @Override
+    public void run() {
+        if(HaltChecker.willHalt(program)) {
+            // Entrar en bucle infinito
+            while(true) {
+                System.out.println("Looping...");
+            }
+        } else {
+            System.out.println("Stopping immediately");
+        }
+    }
+
+    @Override
+    public ProgramPrototype clone() {
+        return new Reverser((Program) program.clone());
+    }
 }
