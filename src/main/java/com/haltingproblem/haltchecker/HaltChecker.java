4dbd8a64e22860b983f63beab2b90e3d60988f23
pabloloz23 <145718438+pabloloz23@users.noreply.github.com>
2025-03-19 11:59:45 +0100

    Update HaltChecker.java

--- src/main/java/com/haltingproblem/haltchecker/HaltChecker.java
+++ src/main/java/com/haltingproblem/haltchecker/HaltChecker.java
@@ -1,4 +1,10 @@
 package com.haltingproblem.haltchecker;
 
+import com.haltingproblem.model.Program;
+
 public class HaltChecker {
+    public static boolean willHalt(Program program) {
+        // Simulación: Si es HaltingProgram, retorna true
+        return program instanceof HaltingProgram;
+    }
 }
