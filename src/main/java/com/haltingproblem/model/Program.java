817a61a9173307eec1160c1519cad03eea8094df
pabloloz23 <145718438+pabloloz23@users.noreply.github.com>
2025-03-19 12:00:23 +0100

    Update Program.java

--- src/main/java/com/haltingproblem/model/Program.java
+++ src/main/java/com/haltingproblem/model/Program.java
@@ -1,4 +1,13 @@
 package com.haltingproblem.model;
 
-public class Program {
+import com.haltingproblem.prototype.ProgramPrototype;
+
+public abstract class Program implements ProgramPrototype {
+    protected int initialValue;
+
+    public void setInitialValue(int value) {
+        this.initialValue = value;
+    }
+
+    public abstract void run();
 }
